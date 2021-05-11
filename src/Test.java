2644a68851e48b454414e975721de5734082b9ae
jiaherng <jiaherng2001@gmail.com>
2021-05-11 19:31:14 +0800

    Feature c

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("feature a");
         System.out.println("Feature b");
+        System.out.println("Feature c");
     }
 }
